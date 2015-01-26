9616d09a9a0629fbff29a5a744882965d747558e
Michael Andersen <m.andersen@cs.berkeley.edu>
2015-01-25 19:37:07 -0800

    re-add latest

--- QDFDashboard/qdfdashboard/__init__.py
+++ QDFDashboard/qdfdashboard/__init__.py
@@ -7,7 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    #config.add_route('latest', '/')
+    config.add_route('latest', '/latest')
     config.add_route('logfile', '/log/{id:[0-9a-z\._]+}')
     config.add_route('archive', '/')
     config.add_route('daylist', '/list/{inst}/{year}/{month}/{day}')
