c9a4f21bd8f55e39ff3fdfc8c0bb804569587746
Michael Andersen <michael@steelcode.com>
2015-05-06 17:54:50 -0700

    Fix logfile name with hyphn

--- QDFDashboard/qdfdashboard/__init__.py
+++ QDFDashboard/qdfdashboard/__init__.py
@@ -8,7 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('latest', '/latest')
-    config.add_route('logfile', '/log/{id:[0-9A-Za-z\._]+}')
+    config.add_route('logfile', '/log/{id:[0-9A-Za-z\-\._]+}')
     config.add_route('archive', '/')
     config.add_route('daylist', '/list/{inst}/{year}/{month}/{day}')
     config.scan()
